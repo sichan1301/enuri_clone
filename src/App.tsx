15857ad574681a72d8f442952d693cec053d6727
sichan <sichan1301@naver.com>
2023-04-09 02:52:12 +0900

    토요일 작업

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,86 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
-
 import './App.css';
 import { Category } from './data/data';
+import styled from 'styled-components';
 
 function App() {
+  const [categoryIdx, setCategoryIdx] = useState(0)
+  const [menusIdx,setMenusIdx] = useState(0)
+  const [menuIdx, setMenuIdx] = useState(0)
+  const [subMenuIdx, setSubMenuIdx] = useState(0)
 
-  
-  // const handleMouseEnter = () => {
-  //   setDisplayMenu(true)
-  // }
+  const handleMenuMouseEnter = (e:React.MouseEvent,menusIdx:number,menuIdx:number) => {
+    setMenusIdx(menusIdx)
+    setMenuIdx(menuIdx)
+  }
 
-  // const handleMouseLeave = () => {
-  //   setDisplayMenu(false)
-  // }
 
-  const handleClick = () => {
-
+  const handleSubMenuMouseEnter = (e:React.MouseEvent,subMenuIdx:number) => {
+    setSubMenuIdx(subMenuIdx)
   }
 
   return (
-    <>
-      {Category.map(category => (<><h1>{category.title}</h1>
-        {
-          <ul>
-            {category.menus.map( menu => (<>
-              <h2>{menu.title}</h2>
-              {menu.menu.map(menu => <li>{menu.title}</li>
-              )}
+    <AppSection>
 
-            </>))}
-          </ul>
-        }
-      
-      </>))}
-    </>
+      {Category.map((category,categoryIdx:number) => (<>
+        <h1>{category.title}</h1>
+        {<ul>
+          {category.menus.map( (menus,menusIdx:number) => (<MenuDiv>
+            <h3>{menus.title}</h3>
+            {menus.menu.map((menu,menuIdx:number) => <li onMouseEnter = {(e)=>{handleMenuMouseEnter(e,menusIdx,menuIdx)}}>{menu.title}</li>)}
+          </MenuDiv>))}
+        </ul>}</>
+      ))}
+
+      {/* {Category[categoryIdx].menus.map((menus,menusIdx:number) => (<MenuDiv>
+            <h3>{Category[categoryIdx].menus.}</h3>
+            {menus.menu.map((menu,menuIdx:number) => <li onMouseEnter = {(e)=>{handleMenuMouseEnter(e,menusIdx,menuIdx)}}>{menu.title}</li>)}
+          </MenuDiv>))}} */}
+
+      <SubMenuUl>
+        <h3>{Category[0].menus[menusIdx].menu[menuIdx].title}</h3>
+        {Category[0].menus[menusIdx].menu[menuIdx].subMenu.map((subMenu,subMenuIdx:number) => <li onMouseEnter={(e)=>{handleSubMenuMouseEnter(e,subMenuIdx)}}>{subMenu.text}</li>)}
+      </SubMenuUl>
+
+      <SubSubMenuUl>
+        {Category[0].menus[menusIdx].menu[menuIdx].subMenu[subMenuIdx].sub_subMenu.map(sub_subMenu => <li>{sub_subMenu.text}</li>)}
+      </SubSubMenuUl>
+
+      <div>
+        {/* <img src ={require('../public/img/cost.jpg')} /> */}
+        <EtcUl>
+          <li><a href="www.~~.com">여행</a></li>
+          <li><a href="www.~~.com">꽃배달</a></li>
+          <li><a href="www.~~.com">이사</a></li>
+        </EtcUl>
+      </div>
+    </AppSection>
   );
 }
 
 export default App;
+
+const AppSection = styled.section`
+  display: flex;
+`
+
+const MenuDiv = styled.section`
+  display: flex;
+  flex-direction: column;
+`
+
+const SubMenuUl = styled.section`
+
+`
+
+
+const SubSubMenuUl = styled.ul`
+  width:300px;
+`
+
+const EtcUl = styled.ul`
+  display:flex;
+  li{
+    margin:10px;
+  }
+`
--- src/data/data.ts
+++ src/data/data.ts
@@ -430,6 +430,486 @@ export const Category:CategoryType[] = [
         ]
       }
     ]
+  },
+
+  {
+    title:"컴퓨터/노트북/조립PC",
+    ad:[
+      {
+        text:"새로워진 삼성 갤럭시북3 시리즈",
+        link:"www.~.com"
+      },
+      {
+        text:"[G식백과] EXPO 기능은 무엇일까?!",
+        link:"www.~.com"
+      },
+      {
+        text:"PC 성능 업그레이드 SSD 추천!",
+        link:"www.~.com"
+      },
+      {
+        text:"디아블로4를 위한 추천 PC",
+        link:"www.~.com"
+      },
+      {
+        text:"고용량의 콘텐츠 저장 및 빠른 활용 만족 스토리지는?",
+        link:"www.~.com"
+      },
+      {
+        text:"뻔하지 않은 디자인과 기능! 멀티탭 골라보자 :)",
+        link:"www.~.com"
+      }
+    ],
+    imgSrc:["cost.jpg","free.jpg"],
+    menus:[
+      {
+        title:"컴퓨터/모니터/복합기",
+        menu:[
+          {
+            title:"노트북",
+            subMenu:[
+              {
+                text:"전체보기",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"2023년형 노트북",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"LG 그램/울트라",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"삼성 갤럭시북3",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"온라인수업/업무용",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"Intel 12/13세대",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"AMD 라이젠 4/5 세대",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"1kg 미만 초경량",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"MX 그래픽 탑재",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"쿼드로 그래픽 탑재",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"게이밍 노트북",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"RTX 40 시리즈 신규",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"RTX 30 시리즈",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"RTX 20 시리즈",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"GTX 16 시리즈",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"Intel 게이밍",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"AMD 게이밍",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"애플 맥북",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"2023년형",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"2022년형",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"2021년형",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"M1/M2 맥북프로 인기",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"M1/M2 맥북에어 인기",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"Intel 맥북프로",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"Intel 맥북에어",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"맥북",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"Intel 노트북",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"13세대 랩터레이크 신규",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"12세대 엘더레이크",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"11세대 타이거레이크",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"Amd 노트북",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"라이젠 7000 시리즈",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"라이젠 6000 시리즈",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"라이젠 5000 시리즈",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"라이젠 4000 시리즈",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+            ]
+          },
+          {
+            title:"13인치 이하",
+            subMenu:[
+              {
+                text:"전체보기",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"QLED TV",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:" 80인치 이상",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"70~79인치",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"60~69인치",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"50~59인치",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"40~49인치",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"39인치 이하",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"TV스탠드/브라켓",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },            
+              {
+                text:"[브랜드관] 이스트라TV",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },            
+              {
+                text:"[브랜드관] 프리즘TV",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+
+            ]
+          }
+        ]
+      },
+
+      {
+        title:"가전",
+        menu:[
+          {
+            title:"주방가전",
+            subMenu:[
+              {
+                text:"냉장고",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"100만원대 냉장고",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"내 맘대로 컬러체인지",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"김치냉장고",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"1삼성 비스포크",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"LG 오브제컬렉션",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"위니아 딤채",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"2023년형",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"1등급",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"스탠드형",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"뚜껑형",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"소용량",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"업소용",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"도어패널",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"2023년형신규",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"23년형 OLED",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"23년형 네오QLED",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"23년형 QLED",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"23년형 LED",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"80인치 이상",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"98/97인치",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"88/86인치",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"85인치",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"83/82인치",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"70~79인치",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"77인치",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"75인치",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"70인치",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"60~69인치",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"65인치",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"60인치",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"50~59인치",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"58인치",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"55인치",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"50인치",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+            ]
+          },
+          {
+            title:"생활가전",
+            subMenu:[
+              {
+                text:"로봇청소기",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"올인원 로봇청소기",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"흡입+걸레겸용",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"세탁기",
+                link:"www.~.com",
+                sub_subMenu:[
+                  {
+                    text:"일반 세탁기",
+                    link:"www.~.com"
+                  },
+                  {
+                    text:"드럼 세탁기",
+                    link:"www.~.com"
+                  }
+                ]
+              },
+              {
+                text:"건조기/의류관리기",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"청소기",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"다리미/재봉/보풀제거",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"디지털도어록",
+                link:"www.~.com",
+                sub_subMenu:[]
+              },
+              {
+                text:"전화기/무전기",
+                link:"www.~.com",
+                sub_subMenu:[]
+              }
+            ]
+          }
+        ]
+      }
+    ]
   }
 ]
 
